c25d802d34410469b42167a7dc5124ecefa98083
Ariane Joy Nova <arianenova@gmail.com>
2024-08-18 14:56:14 -0400

    random movement part 1

---
+++ Assets/AerisFolder/FlyAI.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class FlyAI : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float radius = 5f;
+
+    Vector3 spawnLocation;
+    Vector3 target;
+
+    enum FlyAIState
+    {
+        RANDOM_MOVEMENT,
+        IDLE
+    }
+
+    FlyAIState aiState = FlyAIState.IDLE;
+
+    void Start()
+    {
+        spawnLocation = this.gameObject.transform.position;
+        aiState = FlyAIState.RANDOM_MOVEMENT;
+    }
+
+    void Update()
+    {
+        RunAI();
+    }
+
+    public virtual void RunAI()
+    {
+        switch (aiState)
+        {
+            case FlyAIState.RANDOM_MOVEMENT:
+                RunRandomMovement();
+                break;
+            case FlyAIState.IDLE:
+                break;
+        }
+    }
+
+    public virtual void RunRandomMovement()
+    {
+        if (target == null || this.transform.position == target)
+            SetRandomTarget();
+        else
+            this.transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), target, speed * Time.deltaTime);
+    }
+
+    void SetRandomTarget()
+    {
+        target = spawnLocation + (Vector3)(radius * UnityEngine.Random.insideUnitCircle);
+    }
+}
